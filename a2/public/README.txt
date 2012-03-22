e20807d8f0d129189f0b3214f198671ccf3a9ae9
Logan Gilmour <logangilmour@gmail.com>
2012-03-21 19:57:58 -0500

    Sweet fuck doing it all again

--- a2/public/README.txt
+++ a2/public/README.txt
@@ -1,16 +1,4 @@
-Assignment 3 - CMPUT 410 Winter 2012
+Assignment 4 - CMPUT 410 Winter 2012
 Logan Gilmour
 
-My Application is running at http://ikno.ws
-
-I modified my previous perl script for this assignment, and added php invoice page.
-
-Since I built a key-value store from hidden-form inputs on the last assignment, I was able to simply change the implementation of key-value store to cookie-based storage to accomodate the cookies requirement.
-
-I Added CGI validation for quantities, emails, and postal-codes as I missed them in the previous assignment. I also added a validation confirmation box that allows the user to cancel the actions they've taken on a product page.
-
-So I wouldn't have to rewrite (in php) the code that stores arbitrary product quantities into the value store, I instead have the main index.cgi perl script submit to itself for the 'checkout' tab, then write a redirect header pointing at [base-url]/invoice.php. This allows my cookies storage logic to stay in the perl script.
-
-Sales tax is just a php associative array mapping to the value of the 'province' field in the store.
-
-The main issue I noted is that php and perl serialize cookie-names slightly differently, so care must be taken when storing from one of them and then reading from another.
+I implemented this assignment in php. 
--- a2/public/parse.php
+++ a2/public/parse.php
@@ -1,5 +1,31 @@
+<HTML>
+<HEAD>
+<TITLE>
+Assignment 4
+</TITLE>
+</HEAD>
+<BODY>
+
 <?php
- 
+
+
+$root = 'catalog';
+
+$old = new DOMDocument;
+$old->load($_GET["url"]);
+
+$creator = new DOMImplementation;
+$doctype = $creator->createDocumentType($root, null, './catalog.dtd');
+$new = $creator->createDocument(null, null, $doctype);
+$new->encoding = "utf-8";
+
+$oldNode = $old->getElementsByTagName($root)->item(0);
+$newNode = $new->importNode($oldNode, true);
+$new->appendChild($newNode);
+
+$new->validate();
+
+
 //Initialize the XML parser
  
 $parser=xml_parser_create();
@@ -8,24 +34,17 @@ $parser=xml_parser_create();
 
 $products = array();
 
-$product;
+$product=NULL;
+$current=NULL;
+$title="";
 
 function start($parser,$element_name,$element_attrs)
  
  {
- 
-  switch($element_name)
- 
-    {
- 
-      case "ID":
- 
-      echo "ID! ";
- 
-      break;
- 
-    }
- 
+	$current = $element_name;
+	if($current=="PRODUCT"){
+		$product=array();
+	} 
  }
  
 //Function to use at the end of an element
@@ -33,13 +52,9 @@ function start($parser,$element_name,$element_attrs)
 function stop($parser,$element_name)
  
   {
- 
-	  switch($element_name){
-	  case "catalog":
-		  echo "done";
-		  break;
-
- 
+	if($current=="PRODUCT"){
+		$products[]=$product;
+	}
   }
  
 //Function to use when finding character data
@@ -47,9 +62,11 @@ function stop($parser,$element_name)
 function char($parser,$data)
  
   {
- 
-    echo $data;
- 
+	if($current=="title"){
+		$title.=$data;
+		return;
+	}
+	$product[$current].=$data;
   }
  
 //Specify element handler
@@ -83,5 +100,26 @@ while ($data=fread($fp,4096))
 //Free the XML parser
  
 xml_parser_free($parser);
- 
+
+function comp($a,$b){
+	if($a['NAME']==$b['NAME']){
+		$av = $a['QUANTITY']*$a['VALUE'];
+		$bv = $b['QUANTITY']*$b['VALUE'];
+		if($ab==$bv){
+			return 0;
+		}
+		return $ab>bv?1:-1;
+	}
+	return $a['NAME']>$b['NAME']?1:-1;
+}
+usort($products,'comp');
+$s = "</TD><TD>";
+echo "<TR><TH>Product$h Name$h Description$h Price$h Quantity$h Image</TH></TR>
+foreach ($products as $p){
+	echo "<TR><TD>".$p['PRODUCT'].$s.$p['NAME'].$s.$p['SPECS'].$s.
+	$p['PRICE'].$s.$p['QUANTITY'].$s.
+	"<IMG SRC='".$p['IMAGE']."'></TD></TR>";
+}
 ?>
+</BODY>
+</HTML>
